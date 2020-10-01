34827c592121e4613f8fa1992122798d997fb9c2
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-30 19:15:19 -0600

    skeleton of homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import { useQuery } from '@apollo/react-hooks';
 
 const Home = () => {
 
   return (
-    <main>
-      <div>
-        <div> Project stuff goes here</div>
+    <main className="container">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <h3>My cards</h3>
+        </div>
+        <div className="col-12">
+          <div>cards will be displayed here</div>
+        </div>
       </div>
     </main>
   );
