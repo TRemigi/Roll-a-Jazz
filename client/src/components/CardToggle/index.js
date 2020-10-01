bc8decaa7fc034cd694e419a178519833a962a51
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-10-01 16:18:28 -0600

    Home, Carousel, and CardToggle complete

---
+++ client/src/components/CardToggle/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThLarge, faClone } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+
+const CardToggle = ({ viewSelected, setViewSelected }) => {
+
+    const handleToggle = () => {
+        setViewSelected(!viewSelected)
+    };
+
+    return (
+        <Button variant="primary" size="sm" onClick={handleToggle} className="rounded-0">
+            {viewSelected ? 
+            (<FontAwesomeIcon icon={faThLarge} size="lg" className="card-toggle flip" />) :
+            (<FontAwesomeIcon icon={faClone}  size="lg" className="card-toggle flip" />)}
+        </Button>
+    )
+};
+
+export default CardToggle;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useQuery } from '@apollo/react-hooks';
-import CardCarousel from '../components/Carousel';
 import { QUERY_CARDS } from '../utils/queries';
+
+// import CardList from '../components/CardList';
+import CardCarousel from '../components/Carousel';
+import CardToggle from '../components/CardToggle';
 
 
 const Home = () => {
+
+  const [viewSelected, setViewSelected] = useState(true);
 
   const testCards = [
     'card1',
@@ -20,12 +25,24 @@ const { loading, data } = useQuery(QUERY_CARDS);
     <main className="container">
       <div className="row justify-content-center">
         <div className="col-12">
-          <h3>My cards</h3>
+          <h3 className="pb-3">My cards</h3>
         </div>
-        <div className="col-12 p-5 m-3 text-center" style={{ backgroundColor: "grey", minHeight: "50vh" }}>
+        <div className="col-12 p-0">
+            <CardToggle
+            viewSelected={viewSelected}
+            setViewSelected={setViewSelected}
+            />
+        </div>
+        <div className="col-12 p-5 mt-0 text-center" style={{ backgroundColor: "grey", minHeight: "50vh" }}>
           { loading &&
           <div> Loading... </div>}
-          <CardCarousel cards={ testCards } />
+          {viewSelected ?
+          // (<CardList cards={ cards } />)
+          (<h3>Card List goes here</h3>)
+          :
+          (<CardCarousel cards={ testCards } />)
+          
+        }
         </div>
       </div>
     </main>
