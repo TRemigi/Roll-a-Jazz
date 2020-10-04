3a5c899dfc2ec1eed4eddd0fdc428bdbf2b55ce7
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-10-04 15:03:51 -0600

    create AddCard component which includes other sub-components

---
+++ client/src/components/AddCard/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { QUERY_USER_CARDS } from '../../utils/queries';
+import { Form, Button } from 'react-bootstrap';
+import ResultsModal from '../SearchResultsModal';
+
+
+const AddCard = () => {
+
+    const [searchCards, { data }] = useLazyQuery(QUERY_USER_CARDS);
+
+    const cards = data?.userCards || [];
+
+    useEffect(() => {
+        if (cards) {
+            console.log(cards);
+        }
+    }, [data]);
+
+    // use State to handle ResultsModal
+    // set initial show state to false
+    const [show, setShow] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        await searchCards({
+            variables: { name: e.target.nameInput.value }
+        });
+
+        setShow(true);
+    };
+
+
+
+
+    return (
+        <>
+        <ResultsModal
+        show={show}
+        setShow={setShow}
+        results={cards}
+        />
+
+            <Form
+            inline
+            className="col-12"
+            onSubmit={handleSubmit}
+            >
+                <Form.Label htmlFor="nameInput" srOnly>
+                    Name
+                </Form.Label>
+                <Form.Control
+                    style={ { width: "90%" } }
+                    className="mb-2 mr-sm-2"
+                    id="nameInput"
+                    placeholder="Enter a name to search for cards"
+                />
+                <Button type="submit" className="mb-2">
+                    Search
+                </Button>
+            </Form>
+        </>
+    )
+};
+
+export default AddCard;
